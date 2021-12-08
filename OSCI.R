5d7f42d780c57c9890a26f23f375268746301003
JBSspc <77463414+JBSspc@users.noreply.github.com>
2021-12-08 14:05:55 -0600

    Graphs pt2

---
+++ OSCI.R
@@ -0,0 +1,33 @@
+library(deSolve)
+# ESPECIFICACION DEL MODELO
+# Guardemos los parámetros como vector
+parameters <- c(b = 0.0013, c = 1, mu = 0.022, v = 0.005, T = 205, R = 50)
+
+# VARIABLES DE ESTADO
+# Guardamos las variables, con su valor inicial, como vector
+state <- c(W = 1, Q = 0)
+
+# MODELADO DE LA ECUACIONES
+Osci <- function(t,state, parameters){
+  with(as.list(c(state, parameters)),{
+    # taza de cambio
+    dW <- (b * (0.5 + (0.5*sin((pi/205)*t))) * R * W) - (mu * W)
+    dQ <- (b * c * (1 - (0.5 + (0.5*sin((pi/205)*t)))) * R * W) - (v * Q) 
+    
+    # Retorna tazas de cambio
+    list(c(dW, dQ))
+    })
+}
+
+# APLICACIÓN DEL MODELO
+# Especificamos el tiempo
+times <-seq(0,205)
+
+# Integramos el modelo
+out <- ode(y = state, times = times, func = Osci, parms = parameters)
+head(out)
+
+# Graficando los resultados
+par(oma = c(0,0,3,0))
+plot(out, xlab = "Días de la temporada", ylab = "% utilización")
+mtext(outer = TRUE, side = 3, "Modelo OSCI", cex = 1.5)
